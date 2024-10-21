574c4bdde51d8f4f83e98795841d04de582cd1f7
Abdul Hannan Malik <hannan@cplusoft.com>
2024-10-21 19:00:44 +0500

    Fixed env issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { PusherService } from './pusher.service';
 
+
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true, // makes the configuration available globally
+  }),],
   controllers: [AppController],
   providers: [PusherService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: [process.env.REACT_APP_FRONTEND || 'http://localhost:3000'],
+    origin: [process.env.REACT_APP_FRONTEND],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // optional, specify allowed methods
     credentials: true, // optional, allow credentials (cookies)
   });
