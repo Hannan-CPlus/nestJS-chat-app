64864c0fa1070a52a6469f005fc868087b65479a
Abdul Hannan Malik <hannan@cplusoft.com>
2024-10-21 19:24:16 +0500

    .env Fixes v2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,10 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 @Controller('api')
 export class AppController {
   constructor(private readonly pusherService: PusherService) {}
-
+  @Get() // This will handle GET requests to the root URL
+  getHome(): string {
+    return 'Server is running!';
+  }
   @Post('messages')
   async messages(
     @Body('username') username: string,
